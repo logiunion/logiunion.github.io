843e44ca5e8053dadf6040a48688f33136f84a9c
Karl Fritsche <karl.fritsche@marini.de>
2021-12-10 20:56:26 +0100

    Add a very rough templating system with bash...

---
+++ _build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for FILE in $(ls *.html); do
+  tmp=${FILE#*_}   # remove prefix ending in "_"
+  LANG=${tmp%.*}   # remove suffix starting with "."
+  if [ "$LANG" == "en" ]; then
+    TARGET=${FILE%_*}
+    TARGET="$TARGET.html"
+  else
+    TARGET=$FILE
+  fi
+  declare -x ACTIVE_$LANG="primary"
+  source $LANG.env
+  export LANG LOGI_NAME LOGI_OPEN_LETTER LOGI_GET_INVOLVED LOGI_SIGN_LETTER LOGI_JOIN_DISCORD LOGI_FILL_SURVEY LOGI_SIGNED
+  export LOGI_CONTENT=$(cat $FILE)
+  envsubst < template.htm > ../$TARGET
+  unset ACTIVE_$LANG
+done
